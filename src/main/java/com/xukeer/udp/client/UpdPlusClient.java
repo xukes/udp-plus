a7d60bb0f081b9620011fe84382c89220d625eeb
xuqiwen <657735138@qq.com>
2021-01-11 11:50:13 +0800

    代码重构

---
+++ src/main/java/com/xukeer/udp/client/UpdPlusClient.java
@@ -0,0 +1,9 @@
+package com.xukeer.udp.client;
+
+/*
+ * @Author xqw
+ * @Description
+ * @Date 11:42 2021/1/11
+ **/
+public class UpdPlusClient {
+}
---
+++ src/main/java/com/xukeer/udp/server/UpdPlusServer.java
@@ -0,0 +1,9 @@
+package com.xukeer.udp.server;
+
+/*
+ * @Author xqw
+ * @Description
+ * @Date 11:47 2021/1/11
+ **/
+public class UpdPlusServer {
+}
